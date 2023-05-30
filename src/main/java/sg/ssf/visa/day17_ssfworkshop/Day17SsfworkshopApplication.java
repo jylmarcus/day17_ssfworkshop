9de1579e01864447459d274ab2c205558cdd7276
jylmarcus <maplecow@gmail.com>
2023-05-30 09:40:47 +0800

    Initial commit

---
+++ src/main/java/sg/ssf/visa/day17_ssfworkshop/Day17SsfworkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.ssf.visa.day17_ssfworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day17SsfworkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day17SsfworkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/ssf/visa/day17_ssfworkshop/Day17SsfworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.ssf.visa.day17_ssfworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day17SsfworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
